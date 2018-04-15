c5579c6c57371a995a442d65fb2226fba3931c1b
khoitd1997 <khoidinhtrinh@gmail.com>
2018-04-15 01:39:33 -0700

    clean up maintain file and add announcement printing for setup

--- shell_script/maintain.sh
+++ shell_script/maintain.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # used for daily maintainance
 
-
 #time for the computer to reboot, based on food court inactive hour
 REBOOT_TIME="24:00" 
+PROJECT_NAME="UCI-DWB"
 
 shutdown -r ${REBOOT_TIME}
 
-# for future maintainance stuffs
-# git -C ~/UCI-Digital-Waste-Bin/ pull
+# MAINTAINANCE CODE
+sudo ufw enable # enable firewall if not enabled
+sudo ifconfig wlan0 up # turn on network
+sleep 20 # give wlan0 time to wake up
 
-#MAINTAINANCE CODE
-sudo ufw enable
-sudo ifconfig wlan0 up
-sudo service ntp restart
+sudo service ntp restart 
 sudo apt-get update
 sudo apt-get dist-upgrade -y 
-sudo timedatectl set-timezone US/Pacific
+sudo timedatectl set-timezone US/Pacific 
+
+# software update
+git -C ~/${PROJECT_NAME}/ pull 
+
 sudo ifconfig wlan0 down
+
+exit 0
--- shell_script/setup.sh
+++ shell_script/setup.sh
@@ -6,13 +6,11 @@ SOFTWARE=" server-xorg xinit ufw ntp gcc chromium-browser unclutter "
 
 VERSION="final"
 PROJECT_NAME="UCI-DWB"
-OS="Raspian_Lite"
 STARTUP_FILE="~/.bashrc" # location of file that would be run when user logins
 DISPLAY_FILE="~/.xinitrc"
 BOOT_CONFIG_FILE="/boot/config.txt"
 MODE="compost"
 
-RED='\33[38;5;0196m'
 CYAN='\033[38;5;087m' #for marking the being of a new sections
 YELLOW='\033[38;5;226m' #for error
 GREEN='\033[38;5;154m' #for general messages
@@ -20,6 +18,7 @@ RESET='\033[0m' #for resetting the color
 set -e 
 
 #-------------------------------------------------------------------------------
+printf "\n{GREEN}Starting setup${RESET}"
 
 # change keyboard layout to make sure the rest of installation is correct
 sudo sed -i '/XKBLAYOUT/d' /etc/default/keyboard
@@ -28,13 +27,15 @@ echo XKBLAYOUT=\"us\" | sudo tee -a /etc/default/keyboard
 #change password from default
 read -t 0.1 100000 unused # discard everything in stdin so far
 read -p  
-echo "Please enter password\n"
+echo "\n{GREEN}Please enter new password${RESET}"
 passwd
 sudo passwd -l root # lock root account
 
+printf "\n{GREEN}Beginning Software Install${RESET}"
 # update and install the necessary software
 sudo apt-get update
 sudo apt-get dist-upgrade -y 
+sudo apt-get update
 sudo apt-get install ${SOFTWARE} -y
 
 # enable firewall
@@ -48,12 +49,22 @@ sudo timedatectl set-timezone US/Pacific
 echo "dtoverlay=pi3-disable-bt" | sudo tee --append ${BOOT_CONFIG_FILE}
 echo "display_rotate=3" | sudo tee --append ${BOOT_CONFIG_FILE}
 
-# make sure we don't write same setting to files too many times
+# make sure we don't write same setting to the  display file too many times
 if [ -e "${DISPLAY_FILE}" ]; then 
 truncate -s 0 ${DISPLAY_FILE}
+else
+touch ${DISPLAY_FILE}
 fi
 
 #-------------------------------------------------------------------------------
+
+printf "\n{GREEN}Beginning Display Configuration${RESET}"
+
+# run this maintain script at startup, everything else run after maintain
+echo "${HOME}/${PROJECT_NAME}/shell_script/maintain.sh &" | tee --append ${STARTUP_FILE}
+
+# replace chromium pref file, TODO: change this to sed in the future
+cp -f ~/UCI-DWB/Preferences_Chromium ${HOME}/.config/chromium/Default/Preferences 
 
 # disable screen blanking
 echo "xset s off" |  tee --append ${DISPLAY_FILE}
@@ -65,16 +76,20 @@ sed -i 's/\"exited_cleanly\": true/' ${HOME}/.config/chromium/Default/Preference
 echo "point-rpi" | tee --append ${DISPLAY_FILE} # move mouse to convenient position
 echo "unclutter -idle 0.001 -root" | tee --append ${DISPLAY_FILE} # hide mouse pointer
 
-# run this script at startup 
-echo "${HOME}/${PROJECT_NAME}/shell_script/maintain.sh &" | tee --append ${STARTUP_FILE}
+
 
 # create symlink for the scale, the number seems to be same for every scale
 echo "ACTION==\"add\",SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"0403\", ATTRS{idProduct}==\"6001\", SYMLINK+=\"SCALE\"" | sudo tee --append /etc/udev/rules.d/99-com.rules
 
-# add to startup file to run the html code at boot
+# add running html file to display file
 echo "chromium-browser --noerrdialogs --kiosk --incognito --allow-file-access-from-files ${HOME}/${PROJECT_NAME}/${MODE}/index.html &" | tee --append ${DISPLAY_FILE}
 
+echo "xinit" | tee --append ${STARTUP_FILE} # start x server at login
+
 sudo apt autoremove -y
+# change branch upstream source
+git -C ~/${PROJECT_NAME}/ branch --set-upstream-to release origin/release 
+
 printf "\n{GREEN}Setup done, the system will reboot in 5 seconds${RESET}"
 sleep 5
 reboot
