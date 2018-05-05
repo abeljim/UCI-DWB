c0d9d0188a680cee5d792c64884b0533bfffcf31
khoitd1997 <khoidinhtrinh@gmail.com>
2018-05-05 03:13:06 -0700

    fixed select bugs and added settings for read to wait for 6 bytes

--- scale_optimized.h
+++ scale_optimized.h
@@ -9,13 +9,15 @@
 #define ERROR_FLUSH_FAILED -4
 #define ERROR_SCALE_OVERFLOW -5
 
+#define SCALE_TIMEOUT -6
 #define SCALE_WEIGHT_SAME 0
 
 int openScale(FILE *log);
 
-void errorLogging(char *message, FILE *log);
+void errorLogging(char *message, FILE *log, char *code_section);
 
-float readScale(int scale, fd_set *inputSet, struct timeval *timeOut, FILE *log);
+float readScale(int scale, fd_set *inputSet, struct timeval *timeOut,
+                FILE *log);
 
 int closeScale(int scale, FILE *log);
 #endif
