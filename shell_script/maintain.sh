635a4f9c5ccdd91b350c59a559458e2a2f0e59b1
khoitd1997 <khoidinhtrinh@gmail.com>
2018-05-05 00:41:42 -0700

    trying to fix #5
    Remove output suppression of some commands due to no obvious benefits
    Added TOTAL_FAILURE env var to stop rebooting after a number of failures
    Fixed lack of some variables, rename some to lowercase by conventions

--- shell_script/maintain.sh
+++ shell_script/maintain.sh
@@ -13,8 +13,9 @@ reboot_time="24:00"
 project_name="UCI-DWB"
 
 source ./utils.sh
-STARTUP_FILE="/home/${NON_ROOT_USER}/.bashrc"
-
+startup_file="/home/${NON_ROOT_USER}/.bashrc" # NON_ROOT_USER set during initial installation
+chmod u+x startup_file
+non_root_user_dir="/home/${NON_ROOT_USER}"
 #--------------------------------------------------------------
 log "INFO" "MAINTAIN" "Starting Maintainance"
 
@@ -45,12 +46,14 @@ fi
 
 # only launch the script again if the mode is different from last time
 if [ "${new_mode}" != "${MODE}" ]; then
-sed -i "s|^export MODE=.*$|export MODE=${new_mode}|g" ${STARTUP_FILE}
-exec ${STARTUP_FILE} # stop executing this script and relaunch bashrc to update env var MODE
+sed -i "s|^export MODE=.*$|export MODE=${new_mode}|g" ${startup_file}
+exec ${startup_file} # stop executing this script and relaunch bashrc to update env var MODE
 fi
 
 # MAINTAINANCE CODE
-git checkout release # change branch to receive update from release
+git -C ${
+    
+}/${project_name}/ checkout release # change branch to receive update from release
 sudo ufw enable # enable firewall if not enabled
 sudo ifconfig wlan0 up # turn on network
 sleep 20 # give wlan0 time to wake up
@@ -61,22 +64,35 @@ sudo apt-get dist-upgrade -y >> /dev/null
 sudo timedatectl set-timezone US/Pacific >> /dev/null
 
 # software update
-if ! git -C ${NON_ROOT_USER_DIR}/${project_name}/ fetch > /dev/null 2>&1; then 
+if ! git -C ${non_root_user_dir}/${project_name}/ fetch ; then 
     log "ERROR" "UPDATE" "Failed To Fetch"
+    if [ "${TOTAL_FAILURE}" -gt 5 ]; then
+    exit 1 # exit and leave the screen blank so people can contact the team instead of infinite reboot
+    fi
+    let "TOTAL_FAILURE=TOTAL_FAILURE+1"
+    sed -i "s|^export TOTAL_FAILURE=.*$|export TOTAL_FAILURE=${TOTAL_FAILURE}|g" ${startup_file}
     reboot
 fi
 # only pull and rerun stuffs if there is update
 if [ $(git rev-list  --count origin/release...release) > 0 ]; then
 log "INFO" "UPDATE" "Found Upates"
-if git -C ${NON_ROOT_USER_DIR}/${project_name}/ pull > /dev/null 2>&1 ; then
+if git -C ${non_root_user_dir}/${project_name}/ pull ; then
 log "INFO" "UPDATE" "Finished Applying Update"
-exec ${STARTUP_FILE}
+exec ${startup_file}
 else
 log "ERROR" "UPDATE" "Error updating"
+if [ "${TOTAL_FAILURE}" -gt 5 ]; then
+    exit 1 # exit and leave the screen blank so people can contact the team instead of infinite reboot
+fi
+let "TOTAL_FAILURE=TOTAL_FAILURE+1"
+sed -i "s|^export TOTAL_FAILURE=.*$|export TOTAL_FAILURE=${TOTAL_FAILURE}|g" ${startup_file}
+reboot
+
 fi
 fi
 
 sudo ifconfig wlan0 down
+sed -i "s|^export TOTAL_FAILURE=.*$|export TOTAL_FAILURE=0|g" ${startup_file} # reset total failures to 0
 log "INFO" "MAINTAIN" "Finish Maintainance"
 su - ${NON_ROOT_USER} # change to non-root user for the rest of the day
 shutdown -r ${reboot_time}
