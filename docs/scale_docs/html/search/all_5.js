4e358b9d205811d7707393b11a7188b4b7360733
khoitd1997 <khoidinhtrinh@gmail.com>
2018-05-09 20:58:05 -0700

    update doxygen files

--- docs/scale_docs/html/search/all_5.js
+++ docs/scale_docs/html/search/all_5.js
@@ -7,5 +7,6 @@ var searchData=
   ['scale_5fsignature',['SCALE_SIGNATURE',['../scale__optimized_8c.html#abe973f7a54a96bfa13e62343746f6b2e',1,'scale_optimized.c']]],
   ['scale_5futils_2ec',['scale_utils.c',['../scale__utils_8c.html',1,'']]],
   ['scale_5futils_2eh',['scale_utils.h',['../scale__utils_8h.html',1,'']]],
-  ['scalelogging',['scaleLogging',['../scale__utils_8c.html#acc9e23be18d727ed4d10945bdcff0ecd',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *codeSection):&#160;scale_utils.c'],['../scale__utils_8h.html#a3ac776b9e3af6b526576939336c83270',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *code_section):&#160;scale_utils.c']]]
+  ['scalelogging',['scaleLogging',['../scale__utils_8c.html#acc9e23be18d727ed4d10945bdcff0ecd',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *codeSection):&#160;scale_utils.c'],['../scale__utils_8h.html#a3ac776b9e3af6b526576939336c83270',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *code_section):&#160;scale_utils.c']]],
+  ['sendscaleresult',['sendScaleResult',['../scale__optimized_8c.html#acfd015f86d96ddc2993d7a11d39e3f19',1,'sendScaleResult(float scaleResult, char *saveFileName, FILE *log, const char *mode):&#160;scale_optimized.c'],['../scale__optimized_8h.html#acfd015f86d96ddc2993d7a11d39e3f19',1,'sendScaleResult(float scaleResult, char *saveFileName, FILE *log, const char *mode):&#160;scale_optimized.c']]]
 ];
--- docs/scale_docs/html/search/functions_4.js
+++ docs/scale_docs/html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['scalelogging',['scaleLogging',['../scale__utils_8c.html#acc9e23be18d727ed4d10945bdcff0ecd',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *codeSection):&#160;scale_utils.c'],['../scale__utils_8h.html#a3ac776b9e3af6b526576939336c83270',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *code_section):&#160;scale_utils.c']]]
+  ['scalelogging',['scaleLogging',['../scale__utils_8c.html#acc9e23be18d727ed4d10945bdcff0ecd',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *codeSection):&#160;scale_utils.c'],['../scale__utils_8h.html#a3ac776b9e3af6b526576939336c83270',1,'scaleLogging(const char *infoType, const char *message, FILE *log, const char *code_section):&#160;scale_utils.c']]],
+  ['sendscaleresult',['sendScaleResult',['../scale__optimized_8c.html#acfd015f86d96ddc2993d7a11d39e3f19',1,'sendScaleResult(float scaleResult, char *saveFileName, FILE *log, const char *mode):&#160;scale_optimized.c'],['../scale__optimized_8h.html#acfd015f86d96ddc2993d7a11d39e3f19',1,'sendScaleResult(float scaleResult, char *saveFileName, FILE *log, const char *mode):&#160;scale_optimized.c']]]
 ];
